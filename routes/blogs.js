6cc7c6a69aca4274cd8133c8224e95498d2a3cf4
sarfrazarshad <sarfrazarshad@hotmail.com>
2013-12-16 15:28:42 +0000

    blog.js updated

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,8 +1,66 @@
-exports.findAll = function (req,res){
-    res.send([{name: 'blog1'}, {name: 'blog2'}, {name: 'blog3'}]);
+var mongo=require ('mongodb');
+
+var Server = mongo.Server,
+Db = mongo.Db,
+BSON = mongo.BSONPure;
+
+var server= new Server('localhost', 27017, {auto_reconnect:true});
+db= new Db('blogs', server);
+
+db.open(function(err, db){
+    if(!err){
+	console.log("Connected to blog database");
+	db.collection('blogs', {strict:true}, function(err, collection){
+	    if (err){
+		console.log("The eblogs collection doesn't exist. Creating it with samole data...");
+		populateDB();
+	    };
+	    });
+	}
+    });
+
+
+exports.findById= function(req, res){
+    var id=req.params.id;
+    console.log('Retrieving blogs: ' + id);
+    db.collection('blogs', function(err, collection){
+	collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item){
+			   res.send(item);
+			 });
+		  });
 };
 
-exports.findById = function  (req, res){
-    res.send({ id:req.params.id, name: "The Name", description: "description"});
+exports.findAll = function(req, res) {
+    db.collection('blogs', function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            res.send(items);
+        });
+    });
 };
+
+var populateDB = function() {
  
+    var blogs= [
+    {
+        name: "CHATEAU DE SAINT COSME",
+        year: "2009",
+        grapes: "Grenache / Syrah",
+        country: "France",
+        region: "Southern Rhone",
+        description: "The aromas of fruit and spice...",
+        picture: "saint_cosme.jpg"
+    },
+    {
+        name: "LAN RIOJA CRIANZA",
+        year: "2006",
+        grapes: "Tempranillo",
+        country: "Spain",
+        region: "Rioja",
+        description: "A resurgence of interest in boutique vineyards...",
+        picture: "lan_rioja.jpg"
+    }];
+ 
+    db.collection('blogs', function(err, collection) {
+        collection.insert(wines, {safe:true}, function(err, result) {});
+   });
+}
